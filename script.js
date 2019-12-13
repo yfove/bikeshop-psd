e37920b81d54bb913f5f9ccfd0551b7f9eba7f1f
curtis chat <curtis.ch.chat@gmail.com>
2019-12-12 17:18:27 -0700

    added amburgers menu

---
+++ script.js
@@ -0,0 +1,12 @@
+// amburgers
+
+$(function(){
+	
+	$('.hamburger').on('click', function () {
+		$('.hamburger').toggleClass('is-active');
+	});
+
+	$('.hamburger').on('click', function(){
+		$('.topNav ul').toggleClass('active');
+	});
+});
