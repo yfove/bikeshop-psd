eaa5d1ccb6aecaf858886c03d800709dc40303e2
curtis chat <curtis.ch.chat@gmail.com>
2019-12-13 09:01:53 -0700

    add scroll animations

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // amburgers
 
-$(function(){
+$(document).ready(function(){
+
+	// Initiate animate on scroll
+	AOS.init();
 
 	$('.hamburger').on('click', function () {
 		$('.hamburger').toggleClass('is-active');
@@ -9,4 +12,11 @@ $(function(){
 	$('.hamburger').on('click', function(){
 		$('.topNav ul').toggleClass('active');
 	});
+
+	// Initiate smooth scroll
+    $('a').smoothScroll({
+		speed: 1000
+	});
+
+	
 });
